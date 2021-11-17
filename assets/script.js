e613d3244f8630cc66ae8907970c49e3bdcb8818
Fabulosa <fabiscg79@gmail.com>
2021-11-17 15:09:16 -0500

    error checked all bugs

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,33 @@
-
-
-
 var allQuestions = [
-        {
-            question: '<p>Coffee is a popular drink enjoyed by people around the world</p>',
-            options: ['The div is missing an attribute text',
+    {
+        question: '<p>Coffee is a popular drink enjoyed by people around the world</p>',
+        options: ['The div is missing an attribute text',
             'The text content should be wrapped in a p tag',
-             'Divs can not contain text as inmediate descentent',
-             'Nothing is worng'],
-            correct: 'Nothing is wrong.'
-        },
-        {
-            question: 'What is JavaScrip?',
-            options: ['Company','Brand','Programming language','None of the above'],
-            correct: 'Programing Language'
-        },
-        {
-            question: 'What does CSS stands for?',
-            options: ['common Style sheet','Colorful Style Sheet','Computer Style Sheet','Cascading Style Sheet'],
-            correct: 'Cascading Stye Sheet'
-        },
-        {
-            question: 'Arrays in JavaScript can be use to store _____',
-            options: ['Numbers and strings','Other arrays','Booleans','All of the above'],
-            correct:'All of the above.'
-        },
-   
-        {
-            options: ["Cmmas", "Curly Brackets", "Quotes", "Paranthesis"],
-            question: "String values can be enclosed within ____ when being assign to variables.",
-            correct: "Quotes"
-        },
+            'Divs can not contain text as inmediate descentent',
+            'Nothing is wrong'],
+        correct: 'Nothing is wrong'
+    },
+    {
+        question: 'What is JavaScrip?',
+        options: ['Company', 'Brand', 'Programming Language', 'None of the above'],
+        correct: 'Programming Language'
+    },
+    {
+        question: 'What does CSS stands for?',
+        options: ['common Style sheet', 'Colorful Style Sheet', 'Computer Style Sheet', 'Cascading Style Sheet'],
+        correct: 'Cascading Style Sheet'
+    },
+    {
+        question: 'Arrays in JavaScript can be use to store _____',
+        options: ['Numbers and strings', 'Other arrays', 'Booleans', 'All of the above'],
+        correct: 'All of the above'
+    },
+
+    {
+        question: "String values can be enclosed within ____ when being assign to variables.",
+        options: ["Cmmas", "Curly Brackets", "Quotes", "Paranthesis"],
+        correct: "Quotes"
+    },
 ];
 
 
@@ -51,133 +48,132 @@ var score = 0;
 
 
 
-startBtn.addEventListener('click', function(){
-    if( holdTime === 0){
-    holdTime = setInterval(function(){
-        remainingTime--;
-        timeEl.textContent = "Time: " + remainingTime;
-        if(remainingTime <= 0){
-            clearInterval(holdTime);
-            allDone();
-            startBtn.textContent = "all Done!";
-        }
+startBtn.addEventListener('click', function () {
+    if (holdTime === 0) {
+        holdTime = setInterval(function () {
+            remainingTime--;
+            timeEl.textContent = "Time: " + remainingTime;
+            if (remainingTime <= 0) {
+                clearInterval(holdTime);
+                allDone();
+                startBtn.textContent = "all Done!";
+            }
 
-    }, 1000); 
-};
-render (questionIndex);
+        }, 1000);
+    };
+    render(questionIndex);
 });
 
-function render (questionIndex) {
+function render(questionIndex) {
+
+    if (questionIndex > allQuestions.length - 1 || remainingTime === 0) {
+        console.log('quiz is over')
+        allDone()
+        return
+    }
+    console.log(remainingTime);
     quizContainer.innerHTML = "";
     createUl.innerHTML = "";
-    for(var i = 0; i < allQuestions.length; i++){
+    for (var i = 0; i < allQuestions.length; i++) {
         var userQuestion = allQuestions[questionIndex].question;
         var userOptions = allQuestions[questionIndex].options;
         quizContainer.textContent = userQuestion;
     }
 
-    userOptions.forEach(function(newItem){
+    userOptions.forEach(function (newItem) {
         var listEl = document.createElement("li");
         listEl.textContent = newItem;
         quizContainer.appendChild(createUl);
         createUl.appendChild(listEl);
-        listEl.addEventListener("click", (options));
+        listEl.addEventListener("click", options);
     })
 }
 
-function options(event){
-   
+function options(event) {
+
     var element = event.target;
+    var createH3 = document.createElement("h3");
+    createH3.setAttribute("id", "createH3");
 
-    if(element.matches("li")){
-        var createH3 = document.createElement("h3");
-        createH3.setAttribute("id", "createH3");
-
-        if (element.textContent == allQuestions[questionIndex].correct){
-            score++;
-            createH3.textContent = "Correct!" + allQuestions[questionIndex].correct;
-        } else {
-            remainingTime = remainingTime - substractedTime;
-            createH3.textContent = "Wrong!" + allQuestions[questionIndex].correct;
-        } 
-        
-    } 
-    questionIndex++;
-        if(questionIndex >= allQuestions.length){
-            allDone()
-            createH3.textContent = "The Quiz is Over! Your score is " + score + 
-        "/ " + allQuestions.length + " Correct!";
+    if (element.textContent === allQuestions[questionIndex].correct) {
+        score += 20;
+        createH3.textContent = "Correct!" + allQuestions[questionIndex].correct;
     } else {
-        render(questionIndex);
+        remainingTime = remainingTime - substractedTime;
+        createH3.textContent = "Wrong!" + allQuestions[questionIndex].correct;
     }
+
+
+    questionIndex++;
+    render(questionIndex);
     quizContainer.appendChild(createH3);
-           
-      
+
+
 };
 
-function allDone(){
+function allDone() {
     quizContainer.innerHTML = "";
     startBtn.innerHTML = "";
 
-    var createH1 = document.querySelector("h1");
-    createH1.setAttribute("id","createH1");
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
     createH1.textContent = "All Done!";
     quizContainer.appendChild(createH1);
 
-    var pContainer = document.querySelector("p");
+    var pContainer = document.createElement("p");
     pContainer.setAttribute("id", "p");
     quizContainer.appendChild(pContainer);
 
-    if(remainingTime >= 0) {
-        var timeLeft = remainingTime;
-        var createH2 = document.createElement("h2");
-        clearInterval(holdTime);
-        createH3.textContent = "Your score is " + timeLeft;
+    // if (remainingTime >= 0) {
+    //     var timeLeft = remainingTime;
+    //     var createH2 = document.createElement("h2");
+    //     clearInterval(holdTime);
+    //     createH2.textContent = "Your score is " + timeLeft;
 
-        allQuestions.appendChild(createH2);
-    }
+    //     allQuestions.appendChild(createH2);
+    // }
 
     var createLabel = document.createElement("label");
     createLabel.setAttribute("id", "createLabel");
-    createLabel.textContent("Enter Initials:");
-    allQuestions.appendChild(createLabel);
+    createLabel.textContent = "Enter Initials:";
+    quizContainer.appendChild(createLabel);
 
-    var createInput = document.querySelector("input");
+    var createInput = document.createElement("input");
     createInput.setAttribute("type", "text");
     createInput.setAttribute("id", "initials");
     createInput.textContent = "";
 
-    allQuestions.appendChild(createInput);
+    quizContainer.appendChild(createInput);
 
-    var createSubmit = document.querySelector("button");
+    var createSubmit = document.createElement("button");
     createSubmit.setAttribute("type", "submit");
     createSubmit.setAttribute("id", "Submit");
     createSubmit.textContent = "Submit";
 
-    questionsDiv.appendChild(createSubmit);
+    quizContainer.appendChild(createSubmit);
 
-    createSubmit.addEventListener("click", function(){
+    createSubmit.addEventListener("click", function () {
         var userName = createInput.value;
 
-        if (userName === null){
+        if (userName === null) {
             console.log("No value entered!");
         } else {
             var finalScore = {
-                userName:initials,
-                score: timeLeft
+                name: userName,
+                score: score
             }
             console.log(finalScore);
             var allScores = localStorage.getItem("allScores");
-            if(allScores === null){
+            if (allScores === null) {
                 allScores = [];
             } else {
                 allScores = JSON.parse(allScores);
             }
             allScores.push(finalScore);
-            var newScore =JSON.stringify(allScores);
+            var newScore = JSON.stringify(allScores);
             localStorage.setItem("allScores", newScore);
 
-            window.location.replace("/highscore.html");
+            window.location.href = "highscore.html";
         }
     });
 }
--- highscore.js
+++ highscore.js
@@ -1,24 +1,21 @@
-var reset = document.querySelector(".reset");
-var scoreContainer = document.getElementById(".highscore-container");
+var scoreContainer = document.getElementById("highscore-container");
 var goBackBtn = document.querySelector(".go-back-button");
 
-reset.addEventListener("click", function(){
-    localStorage.reset();
-    location.reload();
-});
-var allScores = localStorage.getItem("allScores");
-allScores=JSON.parse(allScores);
+var allScores = JSON.parse(localStorage.getItem("allScores"));
 
-if(allScores !== null) {
-
-    for(var i = 0; i < allScores.length; i++){
-        var createLi = document.querySelector("li");
-        createLi.textContent = allScores[i].userName + " " + allScores[i].score;
-        highscore.appendChild(createLi);
+if (allScores === null) {
+    var h1 = document.createElement("h1");
+    h1.textContent = "No Highscores Yet."
+    scoreContainer.appendChild(h1)
+} else {
+    
+    for (var i = 0; i < allScores.length; i++) {
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].name + " " + allScores[i].score;
+        scoreContainer.appendChild(createLi);
     }
-
 }
 
-goBackBtn.addEventListener("click", function(){
-    window.location.replace("./index.html");
+goBackBtn.addEventListener("click", function () {
+    window.location.href = "index.html";
 });
